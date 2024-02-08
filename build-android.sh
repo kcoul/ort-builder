184b140f1731f592747841cbb91ee4a83ab14ca1
Kieran Coulter <kieran.coulter@gmail.com>
2024-02-07 16:08:24 -0800

    Don't require custom ops list on Linux/Android

--- build-android.sh
+++ build-android.sh
@@ -10,8 +10,6 @@ python3 onnxruntime/tools/ci_build/build.py \
 --parallel \
 --minimal_build \
 --disable_ml_ops --disable_exceptions --disable_rtti \
---include_ops_by_config model.required_operators_and_types.config \
---enable_reduced_operator_type_support \
 --skip_tests
 
 mkdir -p "libs/android_${arch}"
