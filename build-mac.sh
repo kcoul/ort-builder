9ea66595e8bb547f35f21b3b258850ff82ad15bf
kcoul <kieran.coulter@gmail.com>
2024-02-08 17:36:45 -0800

    Enable CoreML, disable config requirement

--- build-mac.sh
+++ build-mac.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 set -euf -o pipefail
 
-ONNX_CONFIG="${1:-model.required_operators_and_types.config}"
+#ONNX_CONFIG="${1:-model.required_operators_and_types.config}"
 CMAKE_BUILD_TYPE=MinSizeRel
 
 build_arch() {
-  ONNX_CONFIG="$1"
-  ARCH="$2"
+  #ONNX_CONFIG="$1"
+  #ARCH="$2"
+  ARCH="$1"
 
   python onnxruntime/tools/ci_build/build.py \
   --build_dir "onnxruntime/build/macOS_${ARCH}" \
   --config=${CMAKE_BUILD_TYPE} \
   --parallel \
-  --minimal_build \
   --apple_deploy_target="10.13" \
   --disable_ml_ops --disable_rtti \
-  --include_ops_by_config "$ONNX_CONFIG" \
-  --enable_reduced_operator_type_support \
   --cmake_extra_defines CMAKE_OSX_ARCHITECTURES="${ARCH}" \
-  --skip_tests
+	--skip_tests \
+	--compile_no_warning_as_error \
+  --use_coreml
 
   BUILD_DIR=./onnxruntime/build/macOS_${ARCH}/${CMAKE_BUILD_TYPE}
 
@@ -37,6 +37,8 @@ build_arch() {
   "${BUILD_DIR}/libonnxruntime_test_utils.a" \
   "${BUILD_DIR}/libonnxruntime_framework.a" \
   "${BUILD_DIR}/libonnxruntime_util.a" \
+  "${BUILD_DIR}/libonnxruntime_coreml_proto.a" \
+  "${BUILD_DIR}/libonnxruntime_providers_coreml.a" \
   "${BUILD_DIR}/_deps/re2-build/libre2.a" \
   "${BUILD_DIR}/_deps/google_nsync-build/libnsync_cpp.a" \
   "${BUILD_DIR}/_deps/protobuf-build/libprotobuf-lite.a" \
@@ -48,8 +50,11 @@ build_arch() {
   "${BUILD_DIR}/_deps/abseil_cpp-build/absl/base/libabsl_raw_logging_internal.a"
 }
 
-build_arch "$ONNX_CONFIG" x86_64
-build_arch "$ONNX_CONFIG" arm64
+#build_arch "$ONNX_CONFIG" x86_64
+#build_arch "$ONNX_CONFIG" arm64
+
+build_arch x86_64
+build_arch arm64
 
 mkdir -p libs/macos-arm64_x86_64
 lipo -create onnxruntime-macos_x86_64-static-combined.a \
