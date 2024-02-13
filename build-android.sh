6c2d59177628127429bfc623256d47e7a7183d1d
kcoul <kieran.coulter@gmail.com>
2024-02-13 12:46:27 -0800

    Enable NNAPI in Android script (WIP)

---
+++ build-android.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+arch="$1" #choose from 'armeabi-v7a', 'arm64-v8a', 'x86', 'x86_64'
+
+python3 onnxruntime/tools/ci_build/build.py \
+--build_dir "onnxruntime/build/android_${arch}" \
+--config=MinSizeRel \
+--android \
+--android_abi="${arch}" \
+--parallel \
+--disable_ml_ops --disable_exceptions --disable_rtti \
+--skip_tests \
+--use_nnapi
