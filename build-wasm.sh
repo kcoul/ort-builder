0bff37a466317f0636100fce754b871a278a03ae
kcoul <kieran.coulter@gmail.com>
2024-03-01 12:17:30 -0800

    Don't require custom ops list for WASM

--- build-wasm.sh
+++ build-wasm.sh
@@ -6,6 +6,4 @@ python3 onnxruntime/tools/ci_build/build.py \
 --parallel \
 --minimal_build \
 --disable_ml_ops --disable_exceptions --disable_rtti \
---include_ops_by_config model.required_operators_and_types.config \
---enable_reduced_operator_type_support \
 --skip_tests
