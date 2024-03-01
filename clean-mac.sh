c46a76406a71bede9ceb9bd2ff5d3a586893bc06
kcoul <kieran.coulter@gmail.com>
2024-03-01 12:15:55 -0800

    Add clean script stub

---
+++ clean-mac.sh
@@ -0,0 +1,3 @@
+python3 onnxruntime/tools/ci_build/build.py --clean --build_dir "onnxruntime/build/macOS_arm64" --config MinSizeRel \
+
+python3 onnxruntime/tools/ci_build/build.py --clean --build_dir "onnxruntime/build/macOS_x86_64" --config MinSizeRel
